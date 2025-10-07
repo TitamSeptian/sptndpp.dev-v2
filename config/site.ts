e8ca61851966fcf8615bf073ff16910e1f5c90ff
Septian Dwi Putra P <44884585+TitamSeptian@users.noreply.github.com>
2025-10-07 17:27:38 +0700

    feat: remove social

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
     title: 'Septian Dwi Putra Pradipta — DevOps Engineer',
-    description: 'Passionate DevOps Engineer based in Bandung, Indonesia, specializing in creating efficient, scalable, and reliable systems.',
+    description: 'Passionate DevOps Engineer, specializing in creating efficient, scalable, and reliable systems.',
     url: 'https://sptndpp.me',
     image: 'https://sptndpp.me/images/profile.jpg',
     nickname: 'sptndpp'

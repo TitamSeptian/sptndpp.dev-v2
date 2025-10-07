bb46a0f68650c5e8ea43a2baf0027dedb93c64bb
Septian Dwi Putra P <44884585+TitamSeptian@users.noreply.github.com>
2025-10-07 17:32:47 +0700

    fix: remove social icon

--- components/grid/contact.tsx
+++ components/grid/contact.tsx
@@ -24,19 +24,6 @@ export default function Contact() {
                     <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:rotate-0' />
                     Contact Me
                 </Button>
-                <div className='inline-flex gap-6'>
-                    {socials.map((social) => (
-                        <Link
-                            key={social.name}
-                            href={social.url}
-                            className='cancel-drag'
-                            aria-label={`My ${social.name}`}
-                            target='_blank'
-                            rel='noreferrer'>
-                            {<social.icon size='1.3rem' />}
-                        </Link>
-                    ))}
-                </div>
             </div>
         </Card>
     );
